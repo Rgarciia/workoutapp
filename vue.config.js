7452f8670f0c116721bd5fc482cd27983ee6592b
Rene Reza Garcia <rene.reza_garcia@hotmail.com>
2023-05-24 17:40:04 -0600

    PWA

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    //themeColor: process.env.VUE_APP_THEME_COLOR,
+    //themeColor: "#000000",
+  }
 })
